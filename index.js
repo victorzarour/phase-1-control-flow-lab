c170d1bf7d4067f3b027bbba6109a0a527f8b186
Victor <vx_zz@hotmail.com>
2022-05-16 19:43:43 -0400

    commit tree 3317af7ebf369b2014e35731d63bc8b5ef27f42e

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rate){
+  if (rate <= 400) {
+    return 'This one is on me!'
+  }
+  else if (rate <= 2000) {
+    return 'That will be twenty bucks.'  
+  }
+  else if (rate <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
